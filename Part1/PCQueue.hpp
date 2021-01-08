e426642f49d9a1d983ea9c563dd6022c156d3668
noamraveh <57923109+noamraveh@users.noreply.github.com>
2021-01-08 07:27:23 +0000

    Add files via upload

--- Part1/PCQueue.hpp
+++ Part1/PCQueue.hpp
@@ -13,9 +13,9 @@ public:
         pthread_mutex_init(&mutex_, nullptr);
     }
     ~PCQueue(){
-        pthread_cond_destroy(&con_c_);
+   /*     pthread_cond_destroy(&con_c_);
         pthread_cond_destroy(&pro_c_);
-        pthread_mutex_destroy(&mutex_);
+        pthread_mutex_destroy(&mutex_); */
     }
 	// Blocks while queue is empty. When queue holds items, allows for a single
 	// thread to enter and remove an item from the front of the queue and return it.
